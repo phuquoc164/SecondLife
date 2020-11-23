8f834b8a6093bc65386b9daf1b4a6b3ec5711c07
phuquoc164 <phuquoc1604@gmail.com>
2020-11-23 11:15:01 +0100

    add InputAutoComplete

---
+++ src/components/AutocompleteInput.js
@@ -0,0 +1,81 @@
+/** React */
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+/** App */
+import {colors} from '../assets/colors';
+
+const AutocompleteInput = (props) => {
+  const numberOptions = props.options.length;
+	const [showSuggestions, setShowSuggestions] = useState(false);
+	const [inputRef, setInputRef] = useState(null);
+
+  const renderItem = (item, index) => {
+    return (
+      <TouchableOpacity
+        style={{
+          paddingVertical: 5,
+          paddingHorizontal: 10,
+          borderColor: colors.gray,
+          borderBottomWidth: index === `${numberOptions - 1}` ? 0 : 1,
+        }}
+        onPress={() => {
+					inputRef.blur();
+					props.onChangeText(item);
+					props.handleAutocomplete && props.handleAutocomplete();
+          setShowSuggestions(false);
+        }}
+				key={index}>
+        <Text>{item}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={props.containerStyle}>
+      <Text style={props.labelStyle}>{props.label}</Text>
+      <View style={{position: 'relative'}}>
+        <TextInput
+					ref={ref => setInputRef(ref)}
+          style={props.inputStyle}
+          placeholder={props.placeholder}
+          placeholderTextColor={colors.gray}
+          onFocus={() => setShowSuggestions(true)}
+          onBlur={() => {
+						setShowSuggestions(false);
+						props.handleAutocomplete && props.handleAutocomplete();
+					}}
+          value={props.value}
+          onChangeText={(data) => props.onChangeText(data)}
+					autoCapitalize={props.autoCapitalize ? props.autoCapitalize : "words"}
+					keyboardType={props.keyboardType ? props.keyboardType : "default"}
+        />
+        {showSuggestions && numberOptions > 0 && (
+          <SafeAreaView
+            style={{
+              backgroundColor: colors.white,
+              borderColor: colors.gray,
+              borderWidth: 1,
+              borderTopWidth: 0,
+              position: 'absolute',
+              top: 30,
+              left: 0,
+              zIndex: 99999,
+              maxHeight: 150,
+              width: '100%',
+            }}>
+            {props.options.map((option, index) => renderItem(option, index))}
+          </SafeAreaView>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default AutocompleteInput;
