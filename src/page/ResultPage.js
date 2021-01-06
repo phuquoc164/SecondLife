4421a207a6a9ba89d30b2e7aa3f7e2ef770ab09b
phuquoc164 <phuquoc1604@gmail.com>
2021-01-06 22:08:39 +0100

    modify the interface of resultpage

--- src/page/ResultPage.js
+++ src/page/ResultPage.js
@@ -11,62 +11,25 @@ const ResultPage = (props) => {
   }
   return (
     <View style={styles.mainScreen}>
-      <TouchableOpacity
-        onPress={props.returnHomePage}
-        style={{position: 'absolute', right: 20, top: 20}}>
-        <Image
-          source={require('../assets/images/cross-black.png')}
-          style={{width: 19.5, height: 19}}
-        />
-      </TouchableOpacity>
-      <View style={{flex: 1, width: '65%', maxWidth: 300}}>
-        <Image
-          source={require('../assets/images/logo.png')}
-          style={{
-            flex: 1,
-            width: null,
-            height: null,
-            resizeMode: 'contain',
-          }}
-        />
-      </View>
-      <View
-        style={{
-          flex: 3,
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-        }}>
-        <View style={{flex: 1}}>
-          <Text style={styles.title}>{props.title}</Text>
-        </View>
-        <View style={{flex: 1, width: '100%', alignSelf: 'center'}}>
+      <View style={{paddingHorizontal: 35}}>
+        <Text style={styles.title}>{props.title}</Text>
+        <View style={{width: '100%', alignSelf: 'center'}}>
           <Image source={icon} style={styles[className]} />
         </View>
-        {props.btnComponent && props.btnComponent()}
       </View>
-      {props.havebtnDeconnecte && (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <TouchableOpacity onPress={props.handleLogout}>
-            <Text style={styles.btnDeconnecte}>Se déconnecter</Text>
-          </TouchableOpacity>
-        </View>
-      )}
+      {props.btnComponent && props.btnComponent()}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   mainScreen: {
-    flex: 1,
-    flexDirection: 'column',
+    height: "100%",
     alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 45,
-    paddingTop: 25,
-    position: "relative"
+    justifyContent: "center",
+    position: "relative",
   },
   title: {
-    flex: 1,
     fontSize: 25,
     textAlign: 'center',
   },
@@ -77,11 +40,7 @@ const styles = StyleSheet.create({
   imageCheck: {
     width: 120,
     height: 100,
-  },
-  btnDeconnecte: {
-    textTransform: 'uppercase',
-    textDecorationLine: 'underline',
-  },
+  }
 });
 
 export default ResultPage;
