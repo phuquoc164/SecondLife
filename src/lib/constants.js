c8f9fd1f71af57bda81fbdc138018030616468be
Phu Quoc DINH <phuquoc.dinh@bienici.com>
2021-06-16 00:10:14 +0200

    fix bug month juillet and change title of catalog page

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 export const STORAGE_USER = "storage_user";
 
-export const monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juiilet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+export const monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 export const DOMAIN = "http://api-tsl.thunderstone.tech";
 export const initialCustomer = {
     firstname: null,
@@ -36,7 +36,7 @@ export const stateDict = {
 
 export const TITLE = {
     clients: ["Clients", "Informations client", "Modifier les informations"],
-    catalog: ["Inventaire Once Again", "Catalogue rayon", "Mes dons\xa0 ♥️"],
+    catalog: ["Catalogue Envoi", "Catalogue rayon", "Mes dons\xa0 ♥️"],
     voucher: ["Bon d'achat", "Historique"],
     profil: ["Compte"],
     products: ["Nouveau Lifer", "Trouver un client", "Informations client", "Ajouter un produit"]
